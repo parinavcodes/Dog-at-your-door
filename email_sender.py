f19f9611960d97dab188b30084fff42a7c7995b8
Parinav <68198328+parinavcodes@users.noreply.github.com>
2021-06-25 00:48:48 +0530

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -10,22 +10,18 @@ from abspath import resource_path
 def emailer():
     load_dotenv()
     emails = []
-    with open(resource_path("email_token.pickle"), 'rb') as token:
+    with open(resource_path("email_token.pickle"), 'rb') as token:  #token is generated from googleapiclient and the credentials are stored in email_token.pickle 
         cred = pickle.load(token)
     with open(resource_path("email_list.txt"), 'rt') as f:
         emails = f.read().rstrip('\n').split('\n')
     service = build('gmail', 'v1', credentials=cred)
 
     mime_message = MIMEMultipart()
-    message = "Jerry at the door"
+    message = "Your dog is at the door"
     mime_message['to'] = ';'.join(emails)
-    mime_message['subject'] = "Jerry at the door"
+    mime_message['subject'] = "Your dog is at the door"
     mime_message.attach(MIMEText(message, 'plain'))
     raw_string = base64.urlsafe_b64encode(mime_message.as_bytes()).decode()
 
     message = service.users().messages().send(
         userId='me', body={'raw': raw_string}).execute()
-    # print(message)
-
-
-# emailer()
